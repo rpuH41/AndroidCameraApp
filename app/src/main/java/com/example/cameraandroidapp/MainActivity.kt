f5082f6dd97f583665fe7ca9fd372e2299a0cf9a
Ivan Liulkovich <ahapxuct09@gmail.com>
2024-11-21 20:24:47 +0300

    Add 3 buttons

--- app/src/main/java/com/example/cameraandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/cameraandroidapp/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.cameraandroidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button1 = findViewById<Button>(R.id.button1)
+        val button2 = findViewById<Button>(R.id.button2)
+        val button3 = findViewById<Button>(R.id.button3)
     }
+
 }
