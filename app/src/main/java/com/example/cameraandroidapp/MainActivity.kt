5c0d876940738e057a3a1f89dd31698092445c2d
Ivan Liulkovich <ahapxuct09@gmail.com>
2024-11-22 20:05:48 +0300

    Addeted code work camera and addeted photo in activity

--- app/src/main/java/com/example/cameraandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/cameraandroidapp/MainActivity.kt
@@ -1,17 +1,81 @@
 package com.example.cameraandroidapp
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var cameraButton: Button
+    private lateinit var imageView: ImageView
+    private val REQUEST_CAMERA_PERMISSION = 1
+    private val REQUEST_IMAGE_CAPTURE = 2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val button1 = findViewById<Button>(R.id.button1)
-        val button2 = findViewById<Button>(R.id.button2)
-        val button3 = findViewById<Button>(R.id.button3)
+
+        cameraButton = findViewById(R.id.button1)
+        imageView = findViewById(R.id.image_view)
+        //Проверяем разрешения для камеры
+        cameraButton.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.CAMERA
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(android.Manifest.permission.CAMERA),
+                    REQUEST_CAMERA_PERMISSION
+                )
+            } else {
+                openCamera()
+            }
+        }
+    }
+    //Захват изображения
+    private fun openCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
+        } else {
+            Toast.makeText(this, "No camera app found", Toast.LENGTH_SHORT).show()
+        }
+    }
+    //Отображение фото
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+     if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK)
+    {
+        val imageBitmap = data?.extras?.get("data") as Bitmap
+        imageView.setImageBitmap(imageBitmap)
+
+    }
+}
+    //Проверка разрешений на камеру
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_CAMERA_PERMISSION -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    openCamera()
+                } else {
+                    Toast.makeText(this, "Camera permission denied", Toast.LENGTH_SHORT).show()
+                }
+                return
+            } else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
     }
 
 }
